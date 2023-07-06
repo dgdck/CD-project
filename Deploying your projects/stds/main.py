bcc78e2593e59e18c6a4020c29d8c28ce530c3bb
dgdck <d.godecke@gmail.com>
2023-07-06 22:39:37 +0200

    first commit

---
+++ Deploying your projects/stds/main.py
@@ -0,0 +1,18 @@
+# Leave these lines untouched
+__winc_id__ = "8c2e6882503c4baa9ce2e050497c3f2f"
+__human_name__ = "stds"
+
+# Your code here
+def main():
+    # TODO: read text from stdin
+
+    # TODO: Filter character given as an argument from the text
+
+    # TODO: Print the result to stdout
+
+    # TODO: Print the total number of removed characters to stderr
+    ...
+
+
+if __name__ == "__main__":
+    main()
