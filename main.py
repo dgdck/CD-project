b13ac8fbe07f84912ce8787b79d9782faabdc70d
dgdck <d.godecke@gmail.com>
2023-08-07 15:08:13 +0200

    test

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 # Main file to be working on
 
+# Import what we need from flask
+from flask import Flask
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+@app.route('/cow')
+def cow():
+    return 'MOoooOo!'
+
+@app.route('/sum')
 def sum(a,b):
     return a + b
-
